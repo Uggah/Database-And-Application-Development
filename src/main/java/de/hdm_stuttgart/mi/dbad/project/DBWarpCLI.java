64b378c4ff2a2f7aa42ad742ed06c352562b4de0
Lucca Greschner <luccagreschner@gmail.com>
2024-03-27 12:30:02 +0100

    Initial dependency setup

---
+++ src/main/java/de/hdm_stuttgart/mi/dbad/project/DBWarpCLI.java
@@ -0,0 +1,37 @@
+package de.hdm_stuttgart.mi.dbad.project;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import lombok.extern.slf4j.XSlf4j;
+import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@XSlf4j
+public class DBWarpCLI implements Callable<Integer> {
+
+    @SuppressWarnings("unused")
+    @CommandLine.Option(names = {"--verbose", "-v"}, description = "Verbose output", defaultValue = "false")
+    private boolean verbose;
+
+    @Override
+    public Integer call() {
+        log.entry();
+        setupLogging();
+
+        return log.exit(0);
+    }
+
+    private void setupLogging() {
+        log.debug("Setting up logging");
+
+        final Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+
+        if (!verbose) {
+            root.setLevel(Level.WARN);
+        }
+
+        root.setLevel(Level.DEBUG);
+    }
+}
--- src/main/java/de/hdm_stuttgart/mi/dbad/project/Main.java
+++ src/main/java/de/hdm_stuttgart/mi/dbad/project/Main.java
@@ -1,7 +1,20 @@
 package de.hdm_stuttgart.mi.dbad.project;
 
+import lombok.extern.slf4j.XSlf4j;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@XSlf4j
+@Command(name = "dbwarp", mixinStandardHelpOptions = true, version = "0.0.1-alpha")
 public class Main {
+
+    /**
+     * Main method
+     * @param args CLI args
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        log.info("Engage!");
+        int exitCode = new CommandLine(new DBWarpCLI()).execute(args);
+        System.exit(exitCode);
     }
 }
