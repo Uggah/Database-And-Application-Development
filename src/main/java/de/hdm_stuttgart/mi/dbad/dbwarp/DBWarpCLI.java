45690be22e657c274503d4eabddc1e3fe4fdaa9b
Kay Knöpfle <kay.knoepfle@exxeta.com>
2024-04-05 08:53:20 +0200

    work around broken validation
    add migration manager

--- src/main/java/de/hdm_stuttgart/mi/dbad/dbwarp/DBWarpCLI.java
+++ src/main/java/de/hdm_stuttgart/mi/dbad/dbwarp/DBWarpCLI.java
@@ -2,10 +2,12 @@ package de.hdm_stuttgart.mi.dbad.dbwarp;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
+import de.hdm_stuttgart.mi.dbad.dbwarp.connection.ConnectionManager;
+import de.hdm_stuttgart.mi.dbad.dbwarp.migration.MigrationManager;
 import de.hdm_stuttgart.mi.dbad.dbwarp.validation.DBWarpCLIValidator;
 import java.util.concurrent.Callable;
 import lombok.extern.slf4j.XSlf4j;
-import org.hibernate.validator.constraints.URL;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.Option;
@@ -20,12 +22,13 @@ public class DBWarpCLI implements Callable<Integer> {
   private CommandSpec spec;
 
   @SuppressWarnings("unused")
-  @URL
+  //ToDo: Write JDBC validator (URL doesn't work)
+  @NotEmpty
   @Parameters(index = "0", description = "JDBC connection URL of source database")
   private String source;
 
   @SuppressWarnings("unused")
-  @URL
+  @NotEmpty
   @Parameters(index = "1", description = "JDBC connection URL of target database")
   private String target;
 
@@ -40,6 +43,9 @@ public class DBWarpCLI implements Callable<Integer> {
     setupLogging();
 
     new DBWarpCLIValidator(spec.commandLine()).validate(this);
+
+    MigrationManager.getInstance().setConnectionManager(new ConnectionManager(source, target));
+    MigrationManager.getInstance().migrate();
 
     return log.exit(0);
   }
