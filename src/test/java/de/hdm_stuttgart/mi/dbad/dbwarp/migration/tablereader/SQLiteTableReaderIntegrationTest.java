9263310155549f009098c3e781d82bdba73afd44
Lucca Greschner <luccagreschner@gmail.com>
2024-04-17 13:08:29 +0200

    Use SQLiteProvider for SQLiteTableReaderIntegrationTest

--- src/test/java/de/hdm_stuttgart/mi/dbad/dbwarp/migration/tablereader/SQLiteTableReaderIntegrationTest.java
+++ src/test/java/de/hdm_stuttgart/mi/dbad/dbwarp/migration/tablereader/SQLiteTableReaderIntegrationTest.java
@@ -6,11 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import de.hdm_stuttgart.mi.dbad.dbwarp.connection.ConnectionManager;
 import de.hdm_stuttgart.mi.dbad.dbwarp.connection.DefaultConnectionManager;
 import de.hdm_stuttgart.mi.dbad.dbwarp.migration.schemareader.SQLiteSchemaReader;
 import de.hdm_stuttgart.mi.dbad.dbwarp.model.column.Column;
 import de.hdm_stuttgart.mi.dbad.dbwarp.model.table.Table;
 import de.hdm_stuttgart.mi.dbad.dbwarp.model.table.TableType;
+import de.hdm_stuttgart.mi.dbad.dbwarp.providers.sql.InitializeDatabase;
+import de.hdm_stuttgart.mi.dbad.dbwarp.providers.sql.SQLiteProvider;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -18,28 +21,14 @@ import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(SQLiteProvider.class)
 class SQLiteTableReaderIntegrationTest {
 
-  private DefaultConnectionManager connectionManager;
-  private Connection connection;
-
-  @BeforeEach
-  void beforeEach() throws SQLException {
-    this.connectionManager = mock(DefaultConnectionManager.class);
-    this.connection = DriverManager.getConnection(
-        "jdbc:sqlite::resource:sqlite/SQLiteSchemaReaderIntegrationTest.db");
-
-    when(connectionManager.getSourceDatabaseConnection()).thenReturn(connection);
-  }
-
-  @AfterEach
-  void afterEach() throws SQLException {
-    this.connection.close();
-  }
-
   @Test
-  void testReadTables() throws SQLException {
+  @InitializeDatabase("sqlite/SQLiteTableReaderIntegrationTest.sql")
+  void testReadTables(final ConnectionManager connectionManager) throws Exception {
     final SQLiteSchemaReader sqLiteTableReader = new SQLiteSchemaReader(connectionManager);
     final List<Table> tables = sqLiteTableReader.readSchema();
 
@@ -70,6 +59,7 @@ class SQLiteTableReaderIntegrationTest {
 
     assertEquals(1, someOtherTestTableColumns.size());
 
+    sqLiteTableReader.close();
   }
 
 }
